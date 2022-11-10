d56b9c2a9b98f147ad08b1736a76179cdfab92f5
chankruze <chankruze@gmail.com>
2022-11-10 20:30:23 +0530

    linked_list: move create linked list of n nodes to utility header
    Signed-off-by: chankruze <chankruze@gmail.com>

--- lib/linked_list.h
+++ lib/linked_list.h
@@ -58,3 +58,36 @@ int count_nodes(struct node *head) {
 
   return count;
 }
+
+void create_linked_list(struct node **head, int n) {
+  // if user input is 0, exit
+  if (n == 0) {
+    printf("exiting...");
+    exit(-1);
+  }
+
+  // temp varible to store user data for new node's data
+  int data;
+  // ask the user for a value to store in first node
+  printf("Enter value for node1's data: ");
+  // store the value to the data variable
+  scanf("%d", &data);
+  // create first node and hold it's address in a temp pointer
+  struct node *temp = create_node(data);
+  // update the head to point to the first node
+  *head = temp;
+
+  // loop n - 1 times
+  for (size_t i = 2; i <= n; i++) {
+    // ask the user for a value to store in {i}th node
+    printf("Enter value for node%ld's data: ", i);
+    //  store the user input to data varible
+    scanf("%d", &data);
+    // create {i}th node with value data
+    struct node *_node = create_node(data);
+    // link prev {i-1}th node's next to {i}th node's address
+    temp->next = _node;
+    // update temp to hold the {i}th node's address
+    temp = _node;
+  }
+}
